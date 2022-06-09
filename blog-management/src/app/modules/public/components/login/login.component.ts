b50eec085b32cd90910fc5bb73195836d8254dc4
eduardo <eduardoamdev@gmail.com>
2022-06-09 18:37:45 +0200

    login form added

--- blog-management/src/app/modules/public/components/login/login.component.ts
+++ blog-management/src/app/modules/public/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
+import { Validators } from "@angular/forms";
 
 @Component({
   selector: "app-login",
@@ -6,7 +9,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required]),
+    });
+
+    this.form.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  save(event: Event) {
+    event.preventDefault();
+    const value = this.form.value;
+    console.log(value);
+  }
 
   ngOnInit(): void {}
 }
