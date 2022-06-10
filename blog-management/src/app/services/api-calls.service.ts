625b66c12526a6690f9f6eb80c856125fe6dbc47
eduardo <eduardoamdev@gmail.com>
2022-06-10 10:22:54 +0200

    env var added

--- blog-management/src/app/services/api-calls.service.ts
+++ blog-management/src/app/services/api-calls.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class ApiCalls {
-  private articlesUrl: string = "http://localhost:3000/articles";
+  private baseUrl: string = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
   getArticles() {
-    return this.http.get(this.articlesUrl);
+    return this.http.get(`${this.baseUrl}/articles`);
   }
 }
