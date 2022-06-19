cf77da4ec20a751db3ed48ac5642c19f2b60e4a0
eduardo <eduardoamdev@gmail.com>
2022-06-19 19:32:37 +0200

    api service refactored

--- blog-management/src/app/services/api-calls.service.ts
+++ blog-management/src/app/services/api-calls.service.ts
@@ -13,11 +13,13 @@ export class ApiCalls {
   login(username: string, password: string) {
     const loginInfo = { username, password };
 
-    const result = "?" + new URLSearchParams(loginInfo).toString();
+    const queriedLoginInfo = "?" + new URLSearchParams(loginInfo).toString();
 
-    const queriedProperties = result.split("?username=")[1].split("&password=");
+    const queriedSeparatedInfo = queriedLoginInfo
+      .split("?username=")[1]
+      .split("&password=");
 
-    const httpQuery: string = `?login=%7B%22username%22%3A%22${queriedProperties[0]}%22,%20%22password%22%3A%22${queriedProperties[1]}%22%7D`;
+    const httpQuery: string = `?login=%7B%22username%22%3A%22${queriedSeparatedInfo[0]}%22,%20%22password%22%3A%22${queriedSeparatedInfo[1]}%22%7D`;
 
     return this.http.get(`${this.baseUrl}/authentication/login${httpQuery}`);
   }
