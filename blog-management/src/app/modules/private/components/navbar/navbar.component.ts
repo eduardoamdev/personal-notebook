7472d35a4cd70420855f9f6750742705c7559831
eduardoamdev <eduardoamdev@gmail.com>
2022-07-16 00:05:57 +0200

    navbar created

---
+++ blog-management/src/app/modules/private/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "initial-page",
+  templateUrl: "./initial-page.component.html",
+  styleUrls: ["./initial-page.component.css"],
+})
+export class InitialPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
