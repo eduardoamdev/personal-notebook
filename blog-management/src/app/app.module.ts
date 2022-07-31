1fb2f238372ff59fb6b60dcca9f47ab491d13c4a
eduardoamdev <eduardoamdev@gmail.com>
2022-07-31 17:06:43 +0200

    private module added to app.module.ts

--- blog-management/src/app/app.module.ts
+++ blog-management/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { AppComponent } from "./app.component";
 import { PublicModule } from "./modules/public/public.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
+import { PrivateModule } from "./modules/private/private.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, PublicModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    PublicModule,
+    PrivateModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
